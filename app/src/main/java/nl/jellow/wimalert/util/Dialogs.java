6ccf06146a3ea13cedc02433eb4f9cef40d236d4
Jelle Fresen <jellefresen@gmail.com>
2016-06-18 22:38:02 +0200

    Add dialog fragment to Dialogs to show an input text dialog

--- app/src/main/java/nl/jellow/wimalert/util/Dialogs.java
+++ app/src/main/java/nl/jellow/wimalert/util/Dialogs.java
@@ -1,5 +1,6 @@
 package nl.jellow.wimalert.util;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
@@ -7,8 +8,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
+import android.text.Editable;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
+
+import nl.jellow.wimalert.MainActivity;
+import nl.jellow.wimalert.R;
 
 /**
  * Created by Jelle on 12-1-2016.
@@ -71,6 +84,60 @@ public class Dialogs {
 		public void onTimeSet(final TimePicker view, final int hourOfDay, final int minute) {
 			Log.i(TAG, "Setting time span " + mPreferenceKey + ": hour=" + hourOfDay + ", minute=" + minute);
 			Prefs.get().setInt(mPreferenceKey, hourOfDay * 60 + minute);
+		}
+
+	}
+
+	public static abstract class TextInputDialog extends DialogFragment {
+
+		protected MainActivity getMainActivity() {
+			final FragmentActivity activity = getActivity();
+			if (activity instanceof MainActivity) {
+				return (MainActivity) activity;
+			}
+			return null;
+		}
+
+		public abstract void configureDialog(AlertDialog.Builder builder);
+		public abstract void configureEditText(EditText editText);
+		public abstract void onPositiveButtonClicked(Editable text);
+
+		@NonNull
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			final Mutable<EditText> inputField = new Mutable<>(null);
+			// Create the dialog
+			final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+			configureDialog(builder);
+			builder.setNegativeButton(R.string.label_cancel, Dialogs.DISMISS_ON_CLICK);
+			builder.setPositiveButton(R.string.label_ok, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(final DialogInterface dialog, final int which) {
+					onPositiveButtonClicked(inputField.value.getText());
+					dialog.dismiss();
+				}
+			});
+			final AlertDialog dialog = builder.create();
+
+			// Inflate the TextInputLayout in which the username can be entered
+			final LayoutInflater inflater = dialog.getLayoutInflater();
+			@SuppressLint("InflateParams")
+			final View content = inflater.inflate(R.layout.dialog_input, null);
+			inputField.value = (EditText) content.findViewById(R.id.input_text);
+			configureEditText(inputField.value);
+			final CharSequence text = inputField.value.getText();
+			inputField.value.setSelection(text == null ? 0 : text.length());
+			inputField.value.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+				@Override
+				public boolean onEditorAction(final TextView v, final int actionId, final KeyEvent event) {
+					dialog.getButton(DialogInterface.BUTTON_POSITIVE).performClick();
+					return true;
+				}
+			});
+
+			dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+			dialog.setView(content);
+			return dialog;
 		}
 
 	}
