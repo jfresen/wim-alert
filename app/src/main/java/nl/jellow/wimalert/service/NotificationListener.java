74013eb5f38e62569a61432961529ae88d322fd9
Jelle Fresen <jellefresen@gmail.com>
2016-10-30 11:10:26 +0100

    Differentiate between notifications of a single contact and multiple contacts

--- app/src/main/java/nl/jellow/wimalert/service/NotificationListener.java
+++ app/src/main/java/nl/jellow/wimalert/service/NotificationListener.java
@@ -4,14 +4,17 @@ import android.app.Notification;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Calendar;
+import java.util.regex.Pattern;
 
 import nl.jellow.wimalert.App;
+import nl.jellow.wimalert.R;
 import nl.jellow.wimalert.util.ViewHierarchy;
 
 /**
@@ -20,6 +23,8 @@ import nl.jellow.wimalert.util.ViewHierarchy;
 public class NotificationListener extends NotificationListenerService {
 
 	private static final String TAG = NotificationListener.class.getSimpleName();
+
+	private Pattern mRegex;
 
 	@Override
 	public void onNotificationPosted(final StatusBarNotification sbn) {
@@ -43,15 +48,40 @@ public class NotificationListener extends NotificationListenerService {
 		if (root == null) {
 			return;
 		}
+		final String regexString = App.getRegexTrigger();
+		if (TextUtils.isEmpty(regexString)) {
+			mRegex = null;
+		} else if (mRegex == null || !mRegex.pattern().equals(regexString)) {
+			mRegex = Pattern.compile(regexString);
+		}
+
 		Log.i(TAG, "=== WHATSAPP MESSAGE ===");
+//		Debug.logD(TAG, ViewHierarchy.dumpViewHierarchy(root, this));
 		final View titleView = root.findViewById(android.R.id.title);
-		boolean multipleMessages = false;
 		if (titleView instanceof TextView) {
 			final CharSequence title = ((TextView) titleView).getText();
 			if (needle.equals(title)) {
-				// Ring the alarm!!!!
 				Log.i(TAG, "Found tracked username in title view");
-				fireAlarm();
+				if (mRegex != null) {
+					// Still need to find a message matching the regex
+					ViewHierarchy.traverse(root, new ViewHierarchy.ViewCallback<TextView>(TextView.class) {
+						@Override
+						public boolean doWithView(@NonNull TextView view) {
+							if (messageMatchesRegex(view)) {
+								Log.i(TAG, "Found matching message in a text view with text " + view.getText());
+								fireAlarm();
+								return false;
+							} else {
+								Log.v(TAG, "No match: " + view.getText());
+							}
+							return true;
+						}
+					});
+				} else {
+					// Ring the alarm!!!!
+					Log.i(TAG, "No regex given, accept the name match");
+					fireAlarm();
+				}
 				return;
 			}
 		}
@@ -59,16 +89,33 @@ public class NotificationListener extends NotificationListenerService {
 		ViewHierarchy.traverse(root, new ViewHierarchy.ViewCallback<TextView>(TextView.class) {
 			@Override
 			public boolean doWithView(@NonNull TextView view) {
+				if (!isMessageView(view)) {
+					return true;
+				}
 				final CharSequence text = view.getText();
 				if (text == null) {
 					return true;
 				}
-				final String name = text.toString().split(":")[0];
+				final String[] split = text.toString().split(":", 2);
+				final String name = split[0];
 				if (needle.equals(name)) {
-					// Ring the alarm!!!!
 					Log.i(TAG, "Found tracked username in other text view");
-					fireAlarm();
-					return false;
+					if (mRegex != null) {
+						if (split.length > 0 && matchesRegex(split[1])) {
+							Log.i(TAG, "Found matching message in a text view with text " + split[1]);
+							fireAlarm();
+							return false;
+						} else {
+							final String msg = split.length > 0 ? split[1] : text.toString();
+							Log.v(TAG, "No match: " + msg);
+						}
+						return true;
+					} else {
+						// Ring the alarm!!!!
+						Log.i(TAG, "No regex given, accept the name match");
+						fireAlarm();
+						return false;
+					}
 				}
 				return true;
 			}
@@ -100,6 +147,35 @@ public class NotificationListener extends NotificationListenerService {
 		} else {
 			return startTime <= currTime || currTime <= endTime;
 		}
+	}
+
+	private boolean messageMatchesRegex(@NonNull final TextView view) {
+		// Skip the title view with the person's name
+		final int id = view.getId();
+		if (id == android.R.id.title || id == R.id.action0) {
+			return false;
+		}
+		return isMessageView(view) && matchesRegex(view.getText());
+	}
+
+	private boolean matchesRegex(final CharSequence text) {
+		if (text == null) {
+			return mRegex == null;
+		}
+		if (mRegex != null) {
+			return mRegex.matcher(text).find();
+		}
+		return true;
+	}
+
+	private boolean isMessageView(@NonNull final TextView view) {
+		final int id = view.getId();
+		if (id == android.R.id.title ||
+				id == R.id.action0 ||
+				id == R.id.text) {
+			return false;
+		}
+		return view.getVisibility() == View.VISIBLE;
 	}
 
 	private void fireAlarm() {
